64b042da894f11d0c7eaadda6934168466cb5817
HammadMehmoodFazal <159707668+HammadFazal@users.noreply.github.com>
2024-04-06 03:30:50 +0500

    push my program to github
    Currency converter program

---
+++ index.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node 
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+
+const currency:any = {
+    USD:1,
+    EUR:0.89,
+    GBP:0.75,
+    INR:74.85,
+    PKR:278.00
+}
+
+let userInput = await inquirer.prompt(
+    [
+        {
+            name:'from',
+            message:'Enter from currency',
+            type:'list',
+            choices:['USD','EUR','GBP','INR','PKR']
+        },
+        {
+            name:'to',
+            message:'Enter to currency',
+            type:'list',
+            choices:['USD','EUR','GBP','INR','PKR']
+        },
+        {
+            name:'amount',
+            message:'Enter amount',
+            type:'number'
+        }
+    ]
+);
+
+let fromAmount = currency[userInput.from];
+let toAmount = currency[userInput.to];
+let amount = userInput.amount;
+let baseAmount_dollarAmount = amount / fromAmount;
+let result = baseAmount_dollarAmount * toAmount;
+console.log(chalk.blue(result));
+
